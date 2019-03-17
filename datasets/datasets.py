6a6006d19d34c756ec6885879a2ca2a2087d796a
cfizette <cfizett1@binghamton.edu>
2019-03-17 09:46:18 -0400

    Added submission dataset

--- datasets/datasets.py
+++ datasets/datasets.py
@@ -25,27 +25,15 @@ def x_to_array(df, feature_cols=FEATURE_COLS):
     for i, idee in enumerate(df.series_id.unique()):
         x_trans[i] = np.array(df[df.series_id == idee][feature_cols]).T
     return x_trans
-    
-
-def load_and_format_data(path):
-    x_train = pd.read_csv(join(path,X_TRAIN))
-    x_test = pd.read_csv(join(path,X_TEST))
-    y_train = pd.read_csv(join(path,Y_TRAIN))
-
-    x_test = x_to_array(x_test)
-    x_train = x_to_array(x_train)
-    y_train = y_train.surface # for now just return unencoded values, let Tensorflow take care of this
-
-    return x_train, y_train, x_test
 
 
-# TODO Implement test/validation set
 class RobotNavDataset(Dataset):
     # TODO add transformations here
     def __init__(self, path_dir, normalize=True):
         self.normalize = normalize
         #self.train, self.labels, self.test = self.load_and_format_data(path_dir)
-        self.train, self.labels = self.load_and_format_data(path_dir)
+        self.data, self.labels = self.load_and_format_data(path_dir)
+        self.label_encoder = LabelEncoder()
         
     def __len__(self):
         return len(self.labels)
@@ -65,18 +53,42 @@ class RobotNavDataset(Dataset):
         #x_test = x_to_array(x_test)
         x_train = x_to_array(x_train)
         
+        # Normalize each channel
         if self.normalize:
             scale_trans = partial(scale, axis=1)
             # TODO: Find better/faster way to do this
             x_train = np.array([scale_trans(samp) for samp in x_train])
             #x_test = np.array([scale_trans(samp) for samp in x_test])
-        
-        y_train = y_train.surface # for now just return unencoded values, let Tensorflow take care of this
-        y_train = LabelEncoder().fit_transform(y_train)
+
+        y_train = self.label_encoder.fit_transform(y_train.surface)
         
         #x_train = torch.tensor(x_train).type(torch.LongTensor)
         #y_train = torch.tensor(y_train).type(torch.LongTensor)
         x_train = torch.tensor(x_train)
         y_train = torch.tensor(y_train)
 
-        return x_train.view((n,1,10,128)), y_train #, torch.tensor(x_test)
+        return x_train.view((n,1,10,128)), y_train 
+
+def SubmissionDataset(Dataset):
+    def __init__(self, path_dir, normalize=True):
+        self.data = self.load_and_format_data(path_dir)
+
+    def __len__(self):
+        return len(self.data)
+        
+    def __getitem__(self, index):
+        X = self.train[index]
+        return X
+
+    def load_and_format_data(self, path):
+        x_test = pd.read_csv(join(path,X_TEST))      
+        n = len(x_test)
+        x_test = x_to_array(x_test)  
+        # Normalize each channel
+        if self.normalize:
+            scale_trans = partial(scale, axis=1)
+            # TODO: Find better/faster way to do this
+            x_test = np.array([scale_trans(samp) for samp in x_test])
+            #x_test = np.array([scale_trans(samp) for samp in x_test])
+        x_test = torch.tensor(x_test)
+        return x_test.view((n,1,10,128))
